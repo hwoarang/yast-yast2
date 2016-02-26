3ec6284affc8bc03db0f80b9a580e47c9b0e51ee
Markos Chandras <mchandras@suse.de>
2016-02-26 15:03:07 +0000

    SuSEFirewall: Move SetInstallPackagesIfMissing to main factory class

--- library/network/src/modules/SuSEFirewall.rb
+++ library/network/src/modules/SuSEFirewall.rb
@@ -442,6 +442,30 @@ module Yast
       @modified || SuSEFirewallServices.GetModified
     end
 
+    # By default Firewall packages are just checked whether they are installed.
+    # With this function, you can change the behavior to also offer installing
+    # the packages.
+    #
+    # @param [Boolean] new_status, 'true' if packages should be offered for installation
+    def SetInstallPackagesIfMissing(new_status)
+      if new_status.nil?
+        Builtins.y2error("Wrong value: %1", new_status)
+        return
+      end
+
+      @check_and_install_package = new_status
+
+      if @check_and_install_package
+        Builtins.y2milestone("Firewall packages will installed if missing")
+      else
+        Builtins.y2milestone(
+          "Firewall packages will not be installed even if missing"
+        )
+      end
+
+      nil
+    end
+
     # Function returns list of maps of known interfaces.
     #
     # **Structure:**
@@ -712,6 +736,10 @@ module Yast
 
       # Are needed packages installed?
       @needed_packages_installed = nil
+
+      # bnc #388773
+      # By default needed packages are just checked, not installed
+      @check_and_install_package = false
 
     end
 
@@ -1264,6 +1292,7 @@ module Yast
     publish variable: :known_firewall_zones, type: "list <string>", private: true
     publish variable: :special_all_interface_zone, type: "string"
     publish variable: :needed_packages_installed, type: "boolean"
+    publish variable: :check_and_install_package, type: "boolean", private: true
     publish function: :GetStartService, type: "boolean ()"
     publish function: :SetStartService, type: "void (boolean)"
     publish function: :GetEnableService, type: "boolean ()"
@@ -1304,6 +1333,7 @@ module Yast
     publish function: :SetServices, type: "boolean (list <string>, list <string>, boolean)"
     publish function: :SetServicesForZones, type: "boolean (list <string>, list <string>, boolean)"
     publish function: :SuSEFirewallIsInstalled, type: "boolean ()"
+    publish function: :SetInstallPackagesIfMissing, type: "void (boolean)"
 
   end
 
