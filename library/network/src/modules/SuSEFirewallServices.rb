d510b8e118e51677b024d2e18069c7d784aa2909
Markos Chandras <mchandras@suse.de>
2016-03-09 18:47:21 +0000

    SuSEFirewallServices: Create basic firewall services factory class
    Create a basic firewall services factory class and rename the
    SuSEFirewallServices one. Moreover, add a stub for the FirewallD
    services class

--- library/network/src/modules/SuSEFirewallServices.rb
+++ library/network/src/modules/SuSEFirewallServices.rb
@@ -41,6 +41,12 @@ module Yast
   end
 
   class SuSEFirewallServicesClass < Module
+    def self.create
+      SuSEFirewall2ServicesClass.new
+    end
+  end
+
+  class SuSEFirewall2ServicesClass < SuSEFirewallServicesClass
     include Yast::Logger
 
     # this is how services defined by package are distinguished
@@ -745,6 +751,9 @@ module Yast
     publish function: :GetPossiblyConflictServices, type: "list <string> ()"
   end
 
-  SuSEFirewallServices = SuSEFirewallServicesClass.new
-  SuSEFirewallServices.main
+  class SuSEFirewalldServicesClass < SuSEFirewallServicesClass
+  end
+
+  SuSEFirewallServices = SuSEFirewallServicesClass.create
+  SuSEFirewallServices.main if SuSEFirewallServices.is_a?(SuSEFirewall2ServicesClass)
 end
